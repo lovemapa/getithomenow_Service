289a4b8cede7699e338734965c86e072f9433ea3
Pawan Kumar <kumar16.pawan@gmail.com>
2020-04-30 00:36:37 +0530

    aded changes

---
+++ server/app/payment/paymentRoute/paymentRoute.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const adminController = require('../paymentController/authorize')
+const CONSTANT = require('../../../constant')
+const multer = require('multer');
+const auth = require('../../auth/configuration')
+
+
+
+
+const payment = express.Router()
+
+
+//Make payment
+
+payment.route('/makePayment')
+    .post((req, res) => {
+        adminController.makePayment(req.body, (result) => {
+            console.log(result);
+
+        }).then(result => {
+            return res.json({
+                success: CONSTANT.TRUE,
+                data: result,
+                message: CONSTANT.PAYMENTSUCCESS,
+
+            })
+        }).catch(error => {
+            console.log(error);
+
+            return res.json({ message: error, success: CONSTANT.FALSE })
+        })
+
+    })
+
+
+module.exports = payment
