469b7b3319c5eb24f62661459304c1a6eb744d32
Pawan Kumar <kumar16.pawan@gmail.com>
2020-05-09 23:54:58 +0530

    added changes in update profile

--- server/app/user/userControllers/userController.js
+++ server/app/user/userControllers/userController.js
@@ -174,8 +174,9 @@ class userModule {
                 if (commonFunctions.compareHash(data.password, pass.password)) {
                     reject("New password cannot be same as old Password")
                 }
-                else if (data.password && data.oldPassword && commonFunctions.compareHash(data.oldPassword, pass.password) === false) {
-                    reject("Old password is wrong")
+                else if (data.password && data.oldPassword) {
+                    if (commonFunctions.compareHash(data.oldPassword, pass.password) === false)
+                        reject("Old password is wrong")
                 }
                 else {
 
